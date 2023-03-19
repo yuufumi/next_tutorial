614eef55ac2acb088080ab322203642bef305123
Youcef BENALI <105224275+yuufumi@users.noreply.github.com>
2023-03-20 00:17:37 +0100

    pages loaded, following tutorial till the last

--- userslist/pages/users/[id].js
+++ userslist/pages/users/[id].js
@@ -1,7 +1,36 @@
-const  Details = () => {
+export const getStaticPaths = async() => {
+const resp = await fetch("https://jsonplaceholder.typicode.com/users");
+const data = await resp.json();
+
+const paths = data.map(user =>{
+    return {
+        params: { id: user.id.toString() }
+    }
+})
+
+return {
+    paths,
+    fallback: false
+}
+}
+
+export const getStaticProps = async(context) => {
+
+    const id = context.params.id
+    const response = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
+    const data = await response.json();
+    return {
+        props: { user: data }
+    }
+}  
+
+const  Details = ({ user }) => {
     return (  
         <div>
-            <h1>Details page</h1>
+            <h1>{user.name}</h1>
+            <p>{user.email}</p>
+            <p>{user.website}</p>
+            <p>{user.address.city}</p>
         </div>
     );
 }
